d21abcc7e65cbec7151906550af67548e8859fb1
Dayanna Rodriguez <l.rosd050115@itses.edu.mx>
2025-06-16 09:21:07 -0600

    Primer proyecto de NetBeans en Spring Boot, Metodos Numericos

---
+++ src/main/java/mx/edu/itses/drs/MetodosNumericos/web/MainController.java
@@ -0,0 +1,20 @@
+package mx.edu.itses.drs.MetodosNumericos.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class MainController {
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+        int i = 1;
+        log.info("Mensaje de salida: {}",i);
+        model.addAttribute("valori", i);
+        return "index";
+    }
+    
+}
